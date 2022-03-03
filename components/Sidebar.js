796a5831cf26a576b3498ec0eccbd1981c6aed0f
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2022-03-04 00:41:37 +0100

    first commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -62,7 +62,7 @@ const Header = styled.div `
   position: sticky;
   top: 0;
   background: white;
-  z-index: 1;
+  z-index: 1; 
   justify-content: space-between;
   align-items: center;
   padding: 15px;
